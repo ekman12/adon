e531e195f3d46628d3fe3b0eab08d7e81941e2a7
Tomek Stacharski <twtstacharski@gmail.com>
2019-04-15 11:58:10 +0100

    added user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_15_105644) do
+ActiveRecord::Schema.define(version: 2019_04_15_105756) do
 
   create_table "adverts", force: :cascade do |t|
     t.text "description"
@@ -19,6 +19,14 @@ ActiveRecord::Schema.define(version: 2019_04_15_105644) do
     t.string "image"
     t.string "cta"
     t.string "user"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
