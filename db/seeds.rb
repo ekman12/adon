1852a09c16213bf78ac2f0ca863bc3b0704d1f1c
Tomek Stacharski <twtstacharski@gmail.com>
2019-04-15 12:05:16 +0100

    added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,102 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+USERS = [
+  "Tomek",
+  "Rishi",
+  "Ana",
+  "Jack",
+]
+
+SUBHEADINGS = [
+  "Potato Head Beach Club Bali",
+  "Crate Cafe",
+  "Milk & Madu Cafe",
+  "Nude Canggu Cafe",
+  "The Loft Canggu",
+  "Machinery Cafe Bali (MCB)",
+  "Debbie Does Salad",
+  "Peloton Supershop",
+  "Bottega Italiana Canggu",
+  "Coffee & Coconuts",
+  "MOTION Cafe"
+]
+
+HEADINGS = [
+  "Great",
+  "Close to you",
+  "Lovely",
+  "This is boring..",
+  "Tasty"
+]
+
+DESCRIPTIONS = [
+  'Developers, designers, and testers often need to add placeholder text to pages to show how content will be displayed on a website. Classic Lorem Ipsum text contains Latin words derived from the works of Cicero. According to lipsum.com, this has been the standard since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
+'I highly suggest that you bookmark these alternatives for all of your future projects.',
+'Real-time; strategize webservices leverage action-items synergies widgets enable generate. Integrate ad delivery harness design extend remix enhance cross-media. Granular, extend weblogs, integrate; sexy expedite e-business drive markets interactive',
+'We offer a range of training packages in a range of subject areas and can offer blended learning opportunities to best meet your needs. If sitting in a room with a trainer and having the opportunity for discussion with other learners sounds good to you then we can offer this.',
+'Jelly sugar plum topping. Jelly beans candy toffee halvah jujubes dragée marzipan. Brownie topping dragée chocolate cupcake. Jujubes bonbon chupa chups donut wypas tiramisu jelly beans. Marzipan jelly-o bear claw pudding toffee jelly powder. Carrot cake sweet lemon drops chocolate cake jujubes wafer soufflé cookie liquorice.',
+'Carrot grape soko wakame plantain pea broccoli rabe desert raisin. Chard cabbage cress gumbo spinach mung bean turnip greens rock melon chicory collard greens bok choy. Wattle seed wakame eggplant soybean quandong garlic prairie turnip swiss chard radish okra.',
+'Bacon ipsum dolor sit amet drumstick salami filet mignon flank. Spare ribs bacon hamburger shankle jerky. Tongue doner pork fatback beef ribs. Salami chicken cow fatback spare ribs, shank short loin capicola kielbasa leberkas bresaola ground round. Shoulder ham hock turducken drumstick pork belly tenderloin doner spare ribs ball tip biltong.',
+"And even if he's a lazy man, and the Dude was certainly that—quite possibly the laziest in Los Angeles County. I don't see any connection to Vietnam, Walter. He lives in North Hollywood on Radford, near the In-and-Out Burger.",
+"Ah, now we see the violence inherent in the system! I don't want to talk to you no more, you empty-headed animal food trough water! Your mother was a hamster and your father smelt of elderberries! Now leave before I am forced to taunt you a second time!",
+"As I stand out here in the wonders of the unknown at Hadley, I sort of realize there’s a fundamental truth to our nature, Man must explore . . . and this is exploration at its greatest. To go places and do things that have never been done before – that’s what living is all about.",
+"Eat stuck in a tree lick biting shed everywhere sleep on your keyboard, catnip litter box sunbathe biting. Lay down in your way toss the mousie lick purr meow, toss the mousie kittens judging you lick chase the red dot climb the curtains scratched.",
+"Messaging bus passive thermal harmonic extended disk. Patch phase particle application force. Floating-point element PC adaptive overflow audio echo extended. Equalized engineered spawned principle."
+]
+
+IMAGES = [
+"https://images.unsplash.com/photo-1521017432531-fbd92d768814?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60.png",
+"https://images.unsplash.com/photo-1521017432531-fbd92d768814?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60.png",
+"https://images.unsplash.com/photo-1466978913421-dad2ebd01d17?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1400&q=60.png",
+"https://images.unsplash.com/photo-1533777857889-4be7c70b33f7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1400&q=60.png",
+"https://images.unsplash.com/photo-1525610553991-2bede1a236e2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1400&q=60.png",
+"https://images.unsplash.com/photo-1428515613728-6b4607e44363?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1400&q=60.png",
+"https://images.unsplash.com/photo-1515668236457-83c3b8764839?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1400&q=60.png",
+"https://images.unsplash.com/photo-1498654896293-37aacf113fd9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1400&q=60.png",
+"https://images.unsplash.com/photo-1491960693564-421771d727d6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1400&q=60.png",
+"https://images.unsplash.com/photo-1531973968078-9bb02785f13d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1100&q=60.png",
+"https://images.unsplash.com/photo-1517638851339-a711cfcf3279?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1100&q=60.png",
+"https://images.unsplash.com/photo-1528605248644-14dd04022da1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1100&q=60.png",
+"https://images.unsplash.com/photo-1506833345857-935a8275b07a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1100&q=60.png",
+"https://images.unsplash.com/photo-1468072114808-903e572b8ead?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1100&q=60.png",
+"https://images.unsplash.com/photo-1416453072034-c8dbfa2856b5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1100&q=60.png",
+"https://images.unsplash.com/photo-1483965907773-04d18dd757c9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1100&q=60.png",
+"https://images.unsplash.com/photo-1485182708500-e8f1f318ba72?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1100&q=60.png",
+"https://images.unsplash.com/photo-1515215316771-2742baa337f4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1100&q=60.png"
+]
+
+p 'deleting previous seeds'
+
+User.delete_all
+Advert.delete_all
+
+p 'deleted seeds'
+
+p '-----'
+
+p 'creating users'
+
+USERS.each do |n|
+  user = User.new(
+        name: n,
+        email: "#{n}@test.com",
+        password: "123456"
+      )
+  user.save!
+end
+
+p 'creating adverts'
+
+40.times do |n|
+  advert = Advert.new(
+        heading: HEADINGS.sample,
+        subheading: SUBHEADINGS.sample,
+        description: DESCRIPTIONS.sample,
+        image: IMAGES.sample,
+        cta: "Learn More",
+        user: USERS.sample
+      )
+  advert.save!
+end
+
+p 'seed finished'
+
+
