3ab4edf61c75a90f130f2e7418ec99910136bb37
Tomek Stacharski <twtstacharski@gmail.com>
2019-04-16 09:21:40 +0100

    controller edited

--- app/controllers/api/v1/adverts_controller.rb
+++ app/controllers/api/v1/adverts_controller.rb
@@ -8,6 +8,21 @@ class Api::V1::AdvertsController < ApplicationController
   end
 
   def create
+    @advert = Advert.new(
+      description: 'TESTING DESCRIPTION FROM CONTROLLER',
+      heading: 'Lovely',
+      subheading: 'MOTION Cafe',
+      image: 'www.test.com/image',
+      cta: 'Learn More',
+      user: 'Ana'
+    )
+    @advert.save
+    render json: advert # see Message.as_json method
+    # if @post.save
+    #   redirect_to places_path
+    # else
+    #   render :new
+    # end
   end
 
   def new
@@ -16,3 +31,13 @@ class Api::V1::AdvertsController < ApplicationController
   def destroy
   end
 end
+
+
+# testAdvert = {
+#    description: "TESTING DESCRIPTION",
+#    heading: 'Lovely',
+#    subheading: 'MOTION Cafe',
+#    image: 'www.test.com/image',
+#    cta: 'Learn More',
+#    user: 'Ana'
+#  }
